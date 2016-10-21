cc1585fe448cda3061be2e6148c2025abbb627d7
Zlatko Gudasić <zlatko.gudasic@gmail.com>
2016-10-21 14:43:44 +0200

    exercise 12

---
+++ source/ex12.py
@@ -0,0 +1,13 @@
+'''
+Already did exercise refactoring in file ex11
+pydoc => module that automatically generates documentation from Python modules
+Usage on this file in windows, assuming file is on path, or you're in the same dir:
+python -m pydoc ex12
+'''
+
+def dummy():
+    """
+    function documentation
+    """
+    print "dummy"
+
