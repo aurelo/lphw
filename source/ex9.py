620508b669bf6b3652ae1918fad2dca5e1325677
Zlatko Gudasić <zlatko.gudasic@gmail.com>
2016-10-21 11:48:36 +0200

    exercise 9

---
+++ source/ex9.py
@@ -0,0 +1,20 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\n...\nSpe\nNov\nDec"
+
+print "Here are the days:", days
+print "Here are the months:", months
+print "Months: %r" % months  # raw printing retains special characters
+
+
+print '''
+There's something going on here
+with the three double quotes
+We'll be able to type as much as we like
+Even 4 lines if we want, or 5, or 6
+'''
+
+print """
+this
+works
+too
+"""
