f1eaaf1f8d75b5a59d625015ff67d57d632e0959
Zlatko Gudasić <zlatko.gudasic@gmail.com>
2016-11-16 13:19:21 +0100

    exercise 24

---
+++ source/ex24.py
@@ -0,0 +1,36 @@
+print "Let's practise everything"
+print 'You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs'
+
+poem = """
+\tThe lovely world
+ with logic so firmly planted
+ cannot discern \n the need of love
+ not comprehend passion for intuition
+ and required an explanation
+ \n\twhere there is none.
+"""
+
+print "------------------------"
+print poem
+print "------------------------"
+
+five = 10 - 2 + 3 - 6
+print "This should be five: %s" % five
+
+
+def secret_formula(started):
+    jelly_beans = started * 500
+    jars_num = jelly_beans / 1000
+    crates_num = jars_num / 100
+    return jelly_beans, jars_num, crates_num
+
+start_point = 1000
+beans, jars, crates = secret_formula(start_point)
+
+print "With a starting point of: %s" % start_point
+print "We'd have %d beans, %d jars and %d crates" % (beans, jars, crates)
+
+start_point /= 10
+
+print "We can also do it like this: "
+print "We'd have %d beans, %d jars and %d crates" % secret_formula(start_point)
